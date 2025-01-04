818537f7ff6c64fad52f607a88c0f48b0f0cf527
Mehmet Eren Akbulut <makbulut@student.42.fr>
2025-01-04 12:14:27 +0000

    [refactor]: Simplify Vagrantfile provisioning scripts
    Replaced inline shell commands with calls to external scripts for better modularity and maintainability.
    - Updated  to standardize provisioning scripts.
    - Replaced detailed provisioning steps in  with calls to  and .
    This reduces redundancy, improves readability, and centralizes script logic for easier updates.

---
+++ p1/scripts/aget.sh
@@ -0,0 +1,7 @@
+      MASTER_IP="192.168.56.110"
+      export K3S_TOKEN=$(cat /vagrant/worker-token.env)
+
+      echo $K3S_TOKEN
+
+      export K3S_URL="https://$MASTER_IP:6443"
+      curl -sfL https://get.k3s.io | sh -
