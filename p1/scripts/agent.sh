224d7c6e1aeaa00c3d7749cfc7e541a54e1c83b8
Mehmet Eren Akbulut <makbulut@student.42.fr>
2025-01-11 13:07:36 +0000

    fix: scriptts

---
+++ p1/scripts/agent.sh
@@ -0,0 +1,8 @@
+MASTER_IP="192.168.56.110"
+export K3S_TOKEN=$(cat /vagrant/confs/worker-token.env)
+
+echo $K3S_TOKEN
+
+export K3S_URL="https://$MASTER_IP:6443"
+curl -sfL https://get.k3s.io | sh -
+rm -rf /vagrant/confs
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+export bashrc_file=/home/vagrant/.bashrc
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode 644 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 --node-ip=192.168.56.110"
 
 curl -sfL https://get.k3s.io | sh -
 
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/confs/worker-token.env
+echo "alias k='kubectl'" >> $bashrc_file
